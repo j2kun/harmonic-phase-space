d38de6e530256eb37cd33d886ab9282cc87e9c03
Jeremy Kun <j2kun@users.noreply.github.com>
2020-02-06 19:53:43 -0800

    make graph update and plot subcurves

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ var margin = {
 };
 var width = 660 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
-var tooltipWidth = 300;
-var tooltipHeight = 300;
-var tooltipAxisPadding = 20;
+var graphWidth = 300;
+var graphHeight = 300;
+var graphAxisPadding = 20;
 
 
 let svg = d3.select("#plot").append("svg")
@@ -29,15 +29,29 @@ let svg = d3.select("#plot").append("svg")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
+var graphContainer = d3.select("body").append("div")
+    .attr("class", "graphContainer")
+    .attr("width", graphWidth + 2 * graphAxisPadding)
+    .attr("height", graphHeight);
+
+var graph = graphContainer.append("svg")
+    .attr("id", "graph")
+    .attr("width", graphWidth + 2 * graphAxisPadding)
+    .attr("height", graphHeight);
+
 var xAxis;
 var yAxis;
 var color;
+var graphXAxis;
+var graphYAxis;
 
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .attr("width", tooltipWidth + 2 * tooltipAxisPadding)
-    .attr("height", tooltipHeight)
-    .style("opacity", 0);
+let plotLine = d3.line()
+  .x(function(d) {
+    return graphXAxis(d.x);
+  })
+  .y(function(d) {
+    return graphYAxis(d.y);
+  });
 
 
 function setupAxes(domain, range) {
@@ -158,95 +172,112 @@ function render() {
         .style("fill", function(d) {
             return color(d.max);
         })
-        .on("mouseover", mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseout", mouseout);
+        .on("mouseover", mousemove);
 
     rects.exit().remove();
 }
 
-function plotTooltipGraph() {
-    let A2 = 0.5;
-    let A3 = 0.7;
-    let p2 = 0.0;
-    let p3 = 0.0;
+function setupGraph() {
+    graphXAxis = d3.scaleLinear().domain([0, 1]).range([0, graphWidth]);
+    graphYAxis = d3.scaleLinear().domain([-3, 3]).range([graphHeight, 0]);
 
-    // Recompute tooltip graph
+    let chart = d3.select("svg#graph")
+        .attr("width", graphWidth + 2 * graphAxisPadding)
+        .attr("height", graphHeight);
+
+    chart.append("g")
+        .attr("transform",
+            "translate(" +
+            2 * graphAxisPadding +
+            "," +
+            graphHeight / 2 +
+            ")")
+        .call(d3.axisBottom(graphXAxis));
+
+    chart.append("g")
+        .attr("transform", "translate(" + 2 * graphAxisPadding + ",0)")
+        .call(d3.axisLeft(graphYAxis));
+
+    chart.append("path")
+      .attr("id", "line")
+      .attr("transform", "translate(" + 2 * graphAxisPadding + ",0)");
+
+    chart.append("path")
+      .attr("id", "curve1")
+      .attr("class", "singleCurve")
+      .attr("transform", "translate(" + 2 * graphAxisPadding + ",0)");
+    chart.append("path")
+      .attr("id", "curve2")
+      .attr("class", "singleCurve")
+      .attr("transform", "translate(" + 2 * graphAxisPadding + ",0)");
+    chart.append("path")
+      .attr("id", "curve3")
+      .attr("class", "singleCurve")
+      .attr("transform", "translate(" + 2 * graphAxisPadding + ",0)");
+}
+
+function updateCurve(amplitude, shift, harmonic, name) {
+    let singleSine = function(x) {
+        return amplitude * Math.sin(harmonic * 2 * Math.PI * (x + shift));
+    };
+
+    let points = graphXAxis.ticks(100).map(function(xi) {
+        return {
+            x: xi,
+            y: singleSine(xi)
+        };
+    });
+
+    d3.selectAll("path#" + name)
+        .each(function (d, i) {
+            if (i === 0) {
+              // put all your operations on the second element, e.g.
+              d3.select(this).datum(points).attr("d", plotLine);
+            }
+          });
+}
+
+function rerenderGraph(params) {
+    let A2 = params.A2;
+    let A3 = params.A3;
+    let p2 = params.p2;
+    let p3 = params.p3;
+
+    // Recompute graph
     let summedSine = function(x) {
         // A_k * sin(k 2pi t + p_k)
         // where A_1 = 1, p_1 = 0
         return (
             Math.sin(1 * 2 * Math.PI * x) +
-            A2 * Math.sin(2 * 2 * Math.PI * x + p2) +
-            A3 * Math.sin(3 * 2 * Math.PI * x + p3)
+            A2 * Math.sin(2 * 2 * Math.PI * (x + p2)) +
+            A3 * Math.sin(3 * 2 * Math.PI * (x + p3))
         );
     };
 
-    let data = groupedPlotData[[A2, A3]];
-    let maxData = data.map(row => row["max"]);
-    let rangeMax = Math.max(...maxData);
-
-    // [-rangeMax, rangeMax] since the curve will be symmetric
-    // and the bounds are +- the max
-    let tooltipX = d3.scaleLinear().domain([0, 1]).range([0, tooltipWidth]);
-    let tooltipY = d3.scaleLinear().domain([-3, 3]).range([tooltipHeight, 0]);
-    let points = tooltipX.ticks(100).map(function(xi) {
+    let points = graphXAxis.ticks(100).map(function(xi) {
         return {
             x: xi,
             y: summedSine(xi)
         };
     });
 
-    let chart = d3.select("svg#tooltipChart")
-        .attr("width", tooltipWidth + 2 * tooltipAxisPadding)
-        .attr("height", tooltipHeight);
+    d3.selectAll("path#line")
+        .each(function (d, i) {
+            if (i === 0) {
+              // put all your operations on the second element, e.g.
+              d3.select(this).datum(points).attr("d", plotLine);
+            }
+          });
 
-
-    chart.append("g")
-        .attr("transform",
-            "translate(" +
-            2 * tooltipAxisPadding +
-            "," +
-            tooltipHeight / 2 +
-            ")")
-        .call(d3.axisBottom(tooltipX));
-
-    chart.append("g")
-        .attr("transform", "translate(" + 2 * tooltipAxisPadding + ",0)")
-        .call(d3.axisLeft(tooltipY));
-
-    var line = d3.line()
-        .x(function(d) {
-            return tooltipX(d.x);
-        })
-        .y(function(d) {
-            return tooltipY(d.y);
-        });
-
-    chart.append("path")
-        .datum(points)
-        .attr("class", "line")
-        .attr("transform", "translate(" + 2 * tooltipAxisPadding + ",0)")
-        .attr("d", line);
+    updateCurve(1, 0, 1, "curve1");
+    updateCurve(A2, p2, 2, "curve2");
+    updateCurve(A3, p3, 3, "curve3");
 }
 
-/* Display the tooltip graph */
-function mouseover() {
-    tooltip.style("opacity", 1);
-}
-
-// where the tooltip previosly contained an image
+// where the graph previosly contained an image
 function mousemove() {
-    tooltip
-        .html('<svg id="tooltipChart"></svg>')
-        .style("left", (d3.event.pageX + 20) + "px")
-        .style("top", (d3.event.pageY + 20) + "px");
-
-    plotTooltipGraph();
-}
-
-function mouseout() {
-    tooltip.style("opacity", 0);
+    let rect = d3.select(this);
+    rerenderGraph(rect.node().__data__);
 }
 
 d3.csv(dataUrl, function(record) {
@@ -279,5 +310,6 @@ d3.csv(dataUrl, function(record) {
     console.log('Done processing data');
     setupAxes(domains["p2"]);
     setupSliders(domains, ["A2", "A3"]);
+    setupGraph();
     render();
 });
